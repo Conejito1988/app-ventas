dfabce00d6daf05792de26e0d517118deb642050
Conejito1988 <rubenzito26@gmail.com>
2026-02-05 21:43:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="App de Ventas", layout="centered")
+
+st.title("📦 App de Ventas")
+st.write("La aplicación se inició correctamente.")
+
+st.success("✅ Todo funciona bien por ahora")
